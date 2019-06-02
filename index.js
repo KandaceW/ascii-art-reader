a2b15911e78ec245bd4e7ac80e8523febb6447ba
Dan van Ammers <49254249+daniel-vanammers@users.noreply.github.com>
2019-06-03 11:48:32 +1200

    this is a commit

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-console.log("good, very")
+const fs = require('fs');
+const readline = require('readline');
+const artChoice = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
 
-function test1(a, b) {
-  return a + b
-}
+console.log("Hello and welcome to our ASCII index.")
+
+artChoice.question("What art do you want displayed?", (answer) =>{
+  if(answer == 0){
+    fs.readFile('./data/kea.txt', 'utf8', (err, data) =>{
+      console.log(data)
+    })
+  }
+  else if(answer == 1){
+    fs.readFile('./data/kiwi.txt', 'utf8', (err, data) =>{
+      console.log(data)
+    })
+  }
+  else if(answer == 2){
+    fs.readFile('./data/nikau.txt', 'utf8', (err, data) =>{
+      console.log(data)
+    })
+  }
+  else if(answer == 3){
+    fs.readFile('./data/pohutukawa.txt', 'utf8', (err, data) =>{
+      console.log(data)
+    })
+  }
+  else{
+    console.log((answer), "is not a choice try 0-3")
+  }
+  
+  artChoice.close()
+})
+
+fs.readdir('./data', 'utf8', function (err, files) {
+  console.log()
+  for (var i = 0; i < files.length; i++) {
+    console.log(i + ": " + files[i])
+
+  }
+})
+
