421b550a50c1052c89064debc1f6c255692089ad
Tom Doesburg <8284791+tom-doesburg@users.noreply.github.com>
2019-06-03 11:50:37 +1200

    Add terminal interface to choose artwork

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-
 module.exports = "index.js";
+
+const fs = require('fs');
+const readline = require('readline');
+
+console.log('Welcome User!')
+console.log('-------------')
+
+
+fs.readdir('./data', function (err, items) {
+  for (let i = 0; i < items.length; i++) {
+    console.log(i + 1 + ': ' + items[i])
+  }
+})
+
+function whichArtwork() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  rl.question('Which artwork would you like to see?\n\n', function (input) {
+    if (input == 1) { console.log('One was pressed') }
+    if (input == 2) { console.log('Two was pressed') }
+    rl.close()
+  })
+}
+
+whichArtwork();
