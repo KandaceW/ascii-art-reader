c28291a22ca5aa3c48c4a1fc019860fdf6546b20
Tess Meyer <49256315+tess-meyer@users.noreply.github.com>
2019-06-03 11:41:24 +1200

    some things work

--- index.js
+++ index.js
@@ -4,13 +4,9 @@ const readline = require('readline')
 //As a user, I'd like to see a welcome message (so that I feel, y'know, welcome).
 console.log("haere mai")
 
-// fs.readFile('index.js', 'utf8', (err, data) => {
-//   if (err) throw err;
-//   console.log(data);
-// });
-
 
 //As a user, I'd like to view a list of ASCII artworks that can be displayed so that I can make my choice.
+
 fs.readdir('./data', "utf8", function (err, data) {
   if (err) throw err;
   console.log("here is a list of ASCII artworks that can be displayed: ");
@@ -20,6 +16,7 @@ fs.readdir('./data', "utf8", function (err, data) {
 })
 
 //As a user, when I enter the number next to an artwork in the list, the artwork will be displayed (so that I can see it!)
+
 
 
 function pressEnter() {
@@ -38,3 +35,29 @@ function pressEnter() {
 }
 
 pressEnter()
+
+
+function loadFile(input) {
+  fs.readdir('./data', "utf8", function (err, data) {
+    // console.log(data)
+    // console.log(input)
+    // console.log(data[input])
+    // fs.readFile('./data', (err, data) => {
+    //   if (err) throw err;
+    //   console.log(data[input]);
+    // });
+
+  })
+}
+
+
+
+//find value of input 
+//check against data array, loop through 
+//return the filename as a new var pass var through a readfile
+
+//example readFile
+fs.readFile('index.js', 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
