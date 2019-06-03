8c6676e0c6b064cd8ab7f1b06afbf40d4c671eeb
Hamish Smith <44103834+hamish-w-smith@users.noreply.github.com>
2019-06-03 12:33:18 +1200

    MVP commit

--- test/test.js
+++ test/test.js
@@ -1,10 +1,16 @@
-var kata = require('../index.js')
+var kata = require("../index.js");
 
-test('greeting not expected', function  () {
-  var name = 'hamish'
-  var actual = 'hello hamish'
-  var expected = 'hello hamish'
-  kata.greeting(name)
-  expect(actual).toEqual(expected)
-})
+test("greeting not expected", function() {
+  var name = "hamish";
+  var actual = "hello hamish";
+  var expected = "hello hamish";
+  kata.greeting(name);
+  expect(actual).toEqual(expected);
+});
 
+test("list not provided", function() {
+  var actual = ["kea", "kiwi", "nikau", "pohutukawa"];
+  var expected = ["kea", "kiwi", "nikau", "pohutukawa"];
+  kata.printList();
+  expect(actual).toEqual(expected);
+});
