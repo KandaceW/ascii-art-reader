767b2592170533fab5f2fa2decb63742270495ef
Dan van Ammers <49254249+daniel-vanammers@users.noreply.github.com>
2019-06-03 12:16:13 +1200

    this is a commit

--- index.js
+++ index.js
@@ -3,43 +3,44 @@ const readline = require('readline');
 const artChoice = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+  terminal: false
 })
 
 console.log("Hello and welcome to our ASCII index.")
 
-artChoice.question("What art do you want displayed?", (answer) =>{
-  if(answer == 0){
-    fs.readFile('./data/kea.txt', 'utf8', (err, data) =>{
+fs.readdir('./data', 'utf8', function (err, files) {
+  console.log()
+  console.log()
+  for (var i = 0; i < files.length; i++) {
+    console.log(i + 1 + ": " + files[i])
+
+  }
+})
+
+artChoice.question("What art do you want displayed?", (answer) => {
+  if (answer == 1) {
+    fs.readFile('./data/kea.txt', 'utf8', (err, data) => {
       console.log(data)
     })
-  }
-  else if(answer == 1){
-    fs.readFile('./data/kiwi.txt', 'utf8', (err, data) =>{
+  } else if (answer == 2) {
+    fs.readFile('./data/kiwi.txt', 'utf8', (err, data) => {
       console.log(data)
     })
-  }
-  else if(answer == 2){
-    fs.readFile('./data/nikau.txt', 'utf8', (err, data) =>{
+  } else if (answer == 3) {
+    fs.readFile('./data/nikau.txt', 'utf8', (err, data) => {
       console.log(data)
     })
-  }
-  else if(answer == 3){
-    fs.readFile('./data/pohutukawa.txt', 'utf8', (err, data) =>{
+  } else if (answer == 4) {
+    fs.readFile('./data/pohutukawa.txt', 'utf8', (err, data) => {
       console.log(data)
     })
-  }
-  else{
-    console.log((answer), "is not a choice try 0-3")
+  } else {
+    console.log((answer), "is not a choice try 1-4")
   }
   
   artChoice.close()
 })
 
-fs.readdir('./data', 'utf8', function (err, files) {
-  console.log()
-  for (var i = 0; i < files.length; i++) {
-    console.log(i + ": " + files[i])
 
-  }
-})
+
 
