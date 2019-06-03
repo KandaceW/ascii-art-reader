0467419619ef920f143cdd16331393d55e7aa6d5
Tess Meyer <49256315+tess-meyer@users.noreply.github.com>
2019-06-03 12:15:36 +1200

    it works ; git push origin

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log("haere mai")
 
 
 //As a user, I'd like to view a list of ASCII artworks that can be displayed so that I can make my choice.
-
+function mainMenu() {
 fs.readdir('./data', "utf8", function (err, data) {
   if (err) throw err;
   console.log("here is a list of ASCII artworks that can be displayed: ");
@@ -14,7 +14,9 @@ fs.readdir('./data', "utf8", function (err, data) {
     console.log(i + ": " + data[i])
   }
 })
+}
 
+mainMenu()
 //As a user, when I enter the number next to an artwork in the list, the artwork will be displayed (so that I can see it!)
 
 
@@ -35,7 +37,7 @@ function pressEnter() {
 }
 
 pressEnter()
-
+ 
 
 function loadFile(input) {
   fs.readdir('./data', "utf8", function (err, data) {
@@ -44,24 +46,35 @@ function loadFile(input) {
     // console.log(data[input])
 
     var file = './data/' + data[input];
-    console.log(file)
+   // console.log(file)
 
     fs.readFile(file, "utf8", (err, data) => {
       if (err) throw err;
       console.log(data);
+      nextChoice()
     });
-
   })
 }
 
+//As a user, I want the menu to display again after I view an artwork so that I can choose another one.
+function nextChoice() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  rl.question('Press enter to continue', function() {
 
 
-//find value of input 
-//check against data array, loop through 
-//return the filename as a new var pass var through a readfile
+    // Call any functions you like here. For example:
+    mainMenu()
 
-//example readFile that WORKS
-// fs.readFile('index.js', 'utf8', (err, data) => {
-//   if (err) throw err;
-//   console.log(data);
-// });
+    rl.question('Which file should I load? ', function (input) {
+
+
+      // Call any functions you like here. For example:
+      loadFile(input)
+      rl.close()
+    })
+  })
+}
